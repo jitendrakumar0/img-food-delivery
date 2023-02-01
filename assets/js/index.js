c609ec45c7aef17b4f53a3913b8152f1da616cec
sushilimg <sushilchoudhary.img@gmail.com>
2023-02-01 17:13:58 +0530

    Work on index page

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,82 @@ if ($("[page-name=landingPage]").length) {
       
 }
 //! ########################### Landing Page End ###########################
+
+//? ###########################  mainSection Page Start  ###########################
+if ($("[page-name=mainSection]").length) {
+  new TomSelect(".singleSelect",{
+    allowEmptyOption: false,
+    create: false
+  });
+
+  $('.newAddedCarousel1').owlCarousel({
+    loop: true,
+    items: 16,
+    margin: 3,
+    nav: true,
+    dots: false,
+    center: false,
+    freeDrag: false,
+    lazyLoad: true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    autoplayHoverPause:true,
+    smartSpeed: 1200,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        576: {
+            items: 2.5,
+        },
+        768: {
+          items: 3,
+        },
+        992: {
+          items: 3,
+        },
+        1200: {
+          items: 8,
+          nav: true,
+      },
+    },
+    navText: ["", ""]
+});
+
+$('.newAddedCarousel2').owlCarousel({
+  loop: true,
+  items: 16,
+  margin: 3,
+  nav: true,
+  dots: false,
+  center: false,
+  freeDrag: false,
+  lazyLoad: true,
+  autoplay:true,
+  autoplayTimeout:1500,
+  autoplayHoverPause:true,
+  smartSpeed: 1200,
+  responsiveClass: true,
+  responsive: {
+      0: {
+          items: 1,
+      },
+      576: {
+          items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      992: {
+        items: 1,
+      },
+      1200: {
+        items: 1,
+        nav: true,
+    },
+  },
+  navText: ["", ""]
+});
+}
+//! ########################### mainSection Page End ###########################
