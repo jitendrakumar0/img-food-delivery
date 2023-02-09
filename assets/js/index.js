ec7daa52f9685a90c58422c157c1ba56889f5b65
sushilimg <sushilchoudhary.img@gmail.com>
2023-02-09 13:25:14 +0530

    work on details page

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,32 @@ if ($("[page-name=landingPage]").length) {
         $(".footer").load("footer.html"); 
         $(".header").load("header.html"); 
       });
-
-      
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
 //! ########################### Landing Page End ###########################
 
 //? ###########################  mainSection Page Start  ###########################
 if ($("[page-name=mainSection]").length) {
-  new TomSelect(".singleSelect",{
+  new TomSelect("#singleSelect",{
     allowEmptyOption: false,
     create: false
   });
